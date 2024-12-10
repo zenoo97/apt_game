d1090ce119b38d41f68fa916ffc27432953b0695
zenoo97 <jaehun4086@naver.com>
2024-12-10 20:34:17 +0900

    [fix] 배포 전 에러 해결(다시)

--- apt_game/pages/utils/supabase.ts
+++ apt_game/pages/utils/supabase.ts
@@ -1,7 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY;
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_SERVICE_ROLE_KEY;
-
-export const supabase = createClient("https://eybfpwveiudecbntszwv.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YmZwd3ZlaXVkZWNibnRzend2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzM3MTc4NCwiZXhwIjoyMDQ4OTQ3Nzg0fQ.sUlrkwobbhAIMdQKkQyQmty2I07JQ8CbU08UlKGvaf0");
+export const supabase = createClient(
+	'https://eybfpwveiudecbntszwv.supabase.co',
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV5YmZwd3ZlaXVkZWNibnRzend2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczMzM3MTc4NCwiZXhwIjoyMDQ4OTQ3Nzg0fQ.sUlrkwobbhAIMdQKkQyQmty2I07JQ8CbU08UlKGvaf0',
+);
